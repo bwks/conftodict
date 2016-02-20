d34717da0e0518b65f48aecb5af68c8717f8eb39
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-02-21 09:49:25 +1100

    added test for non existent file

--- tests/test_conf_to_dict.py
+++ tests/test_conf_to_dict.py
@@ -12,3 +12,7 @@ class TestConfToDict(unittest.TestCase):
 
     def test_initialization_with_correct_file_returns_a_list_of_config(self):
         self.assertIsInstance(self.c.config, list)
+
+    def test_initialization_with_non_existent_file_raises_file_not_found_error(self):
+        from ..conf_to_dict import ConfToDict
+        self.assertRaises(FileNotFoundError, ConfToDict, 'tests/no_file.txt', from_file=True)
